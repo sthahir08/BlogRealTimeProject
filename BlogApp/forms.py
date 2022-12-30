ea28fb1a9ea3050f76b3d977ca923d096ca96572
sthahir08 <sthahir080@gmail.com>
2022-12-30 15:40:32 +0530

    Initial commit

---
+++ BlogApp/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from BlogApp.models import Comment
+
+
+class EmailSendForm(forms.Form):
+    name=forms.CharField()
+    email=forms.EmailField()
+    to=forms.EmailField()
+    comments=forms.CharField(required=False,widget=forms.Textarea)
+
+class CommentForm(forms.ModelForm):
+	class Meta:
+ 		model=Comment
+ 		fields=('name','email','body')
+
+
